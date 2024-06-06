4eb3bbc035613586746b074b806731c1ae00e37d
Eric <inux2012@gmail.com>
2024-06-06 17:53:17 -0300

    some mobile design done, mutation running

--- src/routes/(detail)/community/admin/[uid]/General.svelte
+++ src/routes/(detail)/community/admin/[uid]/General.svelte
@@ -4,6 +4,8 @@
 	import { useUpdateCommunity } from '$lib/hooks/communities';
 	import ColorHash from "color-hash";
 	export let communityUid: string;
+	let name: string;
+	let description: string;
 
 	const updateCommunityMutation = useUpdateCommunity();
 	// use it for updating button text when submitting
@@ -12,7 +14,9 @@
 	async function update() {
 		console.log('updating');
 		let rs = await $updateCommunityMutation.mutateAsync({
-			communityUid
+			uid: communityUid,
+			name,
+			description
 		});
 		console.log('Updated:', rs);
 	}
@@ -22,11 +26,11 @@
 <div class="flex flex-col gap-10">
     <div>
       <Label for="community-name" class="text-base">Community Name</Label>
-      <Input class="mt-2" type="text" id="community-name" placeholder="Argentinean Pink Lizard" required />
+      <Input class="mt-2" type="text" id="community-name" placeholder="Argentinean Pink Lizard" required bind:value={name}/>
     </div>
 	<div class="mt-2">
 		<Label for="description" class="text-base">Short Description<span class="float-right text-sm font-normal text-[#7E8390]">Max. 256 char</span></Label>
-		<Textarea class="mt-2" id="description" placeholder="So gentle, so good" rows="4" name="description" maxlength="256"/>
+		<Textarea class="mt-2" id="description" placeholder="So gentle, so good" rows="4" name="description" maxlength="256" bind:value={description}/>
 	</div>
 <SubmitButton
 	on:click={() => update()}
