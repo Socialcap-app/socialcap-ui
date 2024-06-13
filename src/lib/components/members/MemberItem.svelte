e73473b3d520691f6333b6b3dd4ee050556f97f0
Eric <inux2012@gmail.com>
2024-06-12 21:25:16 -0300

    finally the size of avatar

--- src/lib/components/members/MemberItem.svelte
+++ src/lib/components/members/MemberItem.svelte
@@ -2,6 +2,8 @@
   import Time from "svelte-time/Time.svelte";
   import GradientAvatar from "../common/GradientAvatar.svelte";
   import { getInitials, buildGradient } from "../common/gradient-svg";
+	import { CompressOutline } from 'flowbite-svelte-icons';
+	import ActionFooter from '../../../routes/(grids)/my-votes/ActionFooter.svelte';
 
   export let 
     uid: string = "",
@@ -12,25 +14,35 @@
     isAdmin: boolean = false;
   
   $: initials = getInitials(fullName);
+
+  // let screenWidth = window.innerWidth;
+  // let gradientSize = screenWidth>1024?"lg":"md"
+  
 </script>
 
-<div class="flex items-center justify-between pt-3 pb-3 border-b">
-  <div class="w-12">
+<div class="text-black flex items-center justify-between pt-3 pb-3 lg:border-b">
+  <div class="flex justify-center lg:hidden">
     <GradientAvatar 
       initials={initials} 
-      gradient={buildGradient(initials)} 
+      gradient={buildGradient(initials,"md")} 
+      sizeClasses="w-6 h-6"
+      fontClasses="leading-6 text-[10px]"
     />
   </div>
-  <div class="w-full h-12 ms-4 text-base font-semibold text-black text-start">
+  <div class="flex justify-center max-lg:hidden">
+    <GradientAvatar 
+      initials={initials} 
+      gradient={buildGradient(initials,"lg")} 
+      sizeClasses="w-10 h-10"
+      fontClasses="leading-10 text-base"
+    />
+  </div>
+  <div class="w-full ms-4 text-base text-start">
     <a href={`/person/${personUid}`}>
       {fullName}
     </a>
-    <div class="text-xs text-gray-400 mt-1">
-      Since
-      <Time format="MMM DD, YYYY" timestamp={createdUTC} />
-    </div>
   </div>
-  <div class="text-sm text-black text-end">
-    {personUid.slice(0,6)}...{personUid.slice(-6)}
+  <div class="text-sm font-semibold text-end">
+    {personUid.slice(0,3)}...{personUid.slice(-3)}
   </div>
 </div>
