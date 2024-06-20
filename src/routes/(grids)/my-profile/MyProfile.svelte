7fe0683c67a5fde79df6d58bc01ad716bf063070
Eric <inux2012@gmail.com>
2024-06-20 12:13:24 -0300

    fix font size on general
    add form fields
    
    complete mobile design

--- src/routes/(grids)/my-profile/MyProfile.svelte
+++ src/routes/(grids)/my-profile/MyProfile.svelte
@@ -4,7 +4,7 @@
     import { Select } from "flowbite-svelte";
     
     import { H1, H1Subtitle, ErrorOnFetch } from "$lib/components";
-    import { Badge, Label, Input, Helper }  from 'flowbite-svelte'
+    import { Badge, Label, Input, Helper, Textarea }  from 'flowbite-svelte'
 	  import Time from 'svelte-time';
 	  import { SubmitButton } from '$lib/components';
     import GradientAvatar from "$lib/components/common/GradientAvatar.svelte";
@@ -24,7 +24,7 @@
 
     $: initials = getInitials($profile.data?.fullName??"");
 
-    let editing = false;
+    let editing = true;
 
     const startEdition = (e:MouseEvent) => {
       e.preventDefault()
@@ -33,12 +33,13 @@
 
     const schema = object({
       name: string()
-        .required('Name is required')
+        .required('Name is required'),
+        /*
         .test('verified', 'Community name already exists', async (value) => {
           // if (value === original_name) return true
           const exist = await checkCommunityNameExist(value);
           return !exist as boolean;
-        }),
+        }),*/
       description: string().required('Description is required'),
       // isAdmin: boolean().default(false) TODO: ask about this
     });
@@ -105,14 +106,59 @@
             <div class="text-sm text-gray-400 font-normal">Last update <Time format="hh" timestamp={$profile.data.updatedUTC} /> hour ago</div>     
             <SubmitButton size="md" class="absolute top-4 right-4 py-2 px-4 bg-sc_red hover:bg-sc_red" on:click={(e)=>startEdition(e)}>Edit</SubmitButton>       
             {:else}
-            <form use:form on:submit|stopPropagation|preventDefault class="m-auto lg:max-w-md flex flex-col gap-10">
+            <form use:form on:submit|stopPropagation|preventDefault class="mt-14 px-4 w-full flex flex-col gap-12">
               <div>
-                <Label for="name" class="{$errors.name ? "text-red-500" : "text-base"}">Full name or Alias</Label>
-                <Input class="mt-2 {$errors.name ? "text-red-500" : "text-base"}" type="text" id="name" name="name" placeholder="" required />
+                <Label for="name" class="text-base text-black {$errors.name ? "text-red-500" : ""}">Full name or Alias</Label>
+                <Input class="mt-2 text-sm text-black {$errors.name ? "text-red-500" : ""}" type="text" id="name" name="name" placeholder="Pablo Doe" required />
                 {#if $errors.name && $touched.name}
                 <span class="mt-2 text-sm text-red-500">{$errors.name}</span>
                 {:else}
-                <Helper class="mt-2">Name or alias you would like to show in your profile</Helper>
+                <Helper class="mt-2 text-gray-400">Name or alias you would like to show in your profile</Helper>
+                {/if}
+              </div>
+              <div>
+                <Label for="email" class="text-base text-black {$errors.email ? "text-red-500" : ""}">Email</Label>
+                <Input class="mt-2 text-sm text-black {$errors.email ? "text-red-500" : ""}" type="text" id="email" name="email" placeholder="pablo.doe@gmail.com" required />
+                {#if $errors.email && $touched.email}
+                <span class="mt-2 text-sm text-red-500">{$errors.email}</span>
+                {:else}
+                <Helper class="mt-2 text-gray-400">We need it to contact you. We will never share it with others</Helper>
+                {/if}
+              </div>
+              <div>
+                <Label for="description" class="text-base text-black {$errors.description ? "text-red-500" : ""}">Short bio</Label>
+		            <Textarea class="mt-2 text-sm text-black {$errors.description ? "text-red-500" : ""}" id="description" placeholder="Tell us a bit about yourself" rows="4" name="description" maxlength="256" />
+                {#if $errors.description && $touched.email}
+                <span class="mt-2 text-sm text-red-500">{$errors.description}</span>
+                {:else}
+                <Helper class="mt-2 text-gray-400">A brief description about you that may be of interest to others</Helper>
+                {/if}
+              </div>
+              <div>
+                <Label for="minaaccount" class="text-base text-black {$errors.minaaccount ? "text-red-500" : ""}">Your MINA account</Label>
+                <Input class="mt-2 text-sm text-black {$errors.minaaccount ? "text-red-500" : ""}" type="text" id="minaaccount" name="minaaccount" placeholder="B62qrYipbTfEx5GoJf99uU2iAcW2jgAvnoy1Wrj4Wee" required />
+                {#if $errors.minaaccount && $touched.minaaccount}
+                <span class="mt-2 text-sm text-red-500">{$errors.minaaccount}</span>
+                {:else}
+                <Helper class="mt-2 text-gray-400">This is the MINA account you will use to pay for some services and sign transactions. We will never share it with others.</Helper>
+                {/if}
+              </div>
+              <div>
+                <Label for="telegramaccount" class="text-base text-black {$errors.telegramaccount ? "text-red-500" : ""}">Telegram</Label>
+                <Input class="mt-2 text-sm text-black {$errors.telegramaccount ? "text-red-500" : ""}" type="text" id="telegramaccount" name="telegramaccount" placeholder="@" required />
+                {#if $errors.telegramaccount && $touched.telegramaccount}
+                <span class="mt-2 text-sm text-red-500">{$errors.telegramaccount}</span>
+                {:else}
+                <Helper class="mt-2 text-gray-400">We may use it to contact you. We will never share it with others.</Helper>
+                {/if}
+              </div>
+              <div>
+                <Label for="phone" class="text-base text-black {$errors.phone ? "text-red-500" : ""}">Phone</Label>
+                <Input class="mt-2 text-sm text-black {$errors.phone ? "text-red-500" : ""}" type="text" id="phone" name="phone" placeholder="444-444-444" required />
+                {#if $errors.phone && $touched.phone}
+                <span class="mt-2 text-sm text-red-500">{$errors.phone}</span>
+                {:else}
+                <Helper class="mt-2 text-gray-400">If available we may use it to secure your account. We will never share it with others.</Helper>
                 {/if}
               </div>
               <SubmitButton size="md" class="absolute top-4 right-4 py-2 px-4 bg-sc_red hover:bg-sc_red" on:click={(e)=>saveProfile(e)}>Save changes</SubmitButton>       
