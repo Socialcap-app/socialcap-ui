60cf8686ac4eb47f43361d22d2592bd99c0f02f0
Eric <inux2012@gmail.com>
2024-06-24 00:02:03 -0300

    add mails working

--- src/routes/(home)/home/InviteModal.svelte
+++ src/routes/(home)/home/InviteModal.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-    import { Button, Modal, Label, Input, Helper } from 'flowbite-svelte';
+    import { Button, Modal, Label, Input, Helper, Badge } from 'flowbite-svelte';
+    
 
     import * as yup from 'yup'; 
     import { createForm } from 'felte';
-    import { PlusOutline } from 'flowbite-svelte-icons'
+    import { PlusOutline, CloseOutline } from 'flowbite-svelte-icons'
 
 
     export let openModal = false;
@@ -11,6 +12,21 @@
     let mails:string[] = [];
     let link:string = "";
     // TODO: improve validations
+
+    const addMail = (e:Event) => {
+      e.preventDefault()
+      e.stopPropagation()
+      mails = [...mails, new_mail];
+      new_mail = ""
+      console.log(mails)
+    }
+
+    const removeMail = (e:MouseEvent, index:number) =>{
+      e.preventDefault()
+      e.stopPropagation()
+      console.log(index)  
+      mails = [...mails.slice(0, index), ...mails.slice(index + 1)];
+    }
 
     const schema = yup.object({
       name: yup.string().required('Name is required'),
@@ -46,12 +62,28 @@
             <Label for="name" class="text-base text-black {$errors.name ? "text-red-500" : ""}">Invite</Label>
             <Helper class="mt-2 text-gray-400">Invite teammates to Socialcap</Helper>
             <div class="flex">
-                <Input class="mt-2 text-sm text-black {$errors.name ? "text-red-500" : ""}" type="text" id="name" name="name" placeholder="Pablo Doe" required bind:value={new_mail} />
-                <Button class="bg-primary-500"><PlusOutline/></Button>
+                <Input class="mt-2 text-sm text-black {$errors.name ? "text-red-500" : ""}" 
+                  on:keydown={(e)=>{
+                    if (e.key === "Enter") {
+                      addMail(e)
+                    }
+                  }} 
+                  type="text" id="name" name="name" placeholder="Pablo Doe" required bind:value={new_mail} />
+                <Button class="bg-primary-500" on:click={(e) => addMail(e)}><PlusOutline/></Button>
             </div>
             {#if $errors.name && $touched.name}
             <span class="mt-2 text-sm text-red-500">{$errors.name}</span>
             {/if}
+            <div class="flex flex-wrap gap-3 mt-4">
+            {#each mails as mail, index (index)}
+              <Badge dismissable rounded color="dark" class="bg-gray-400 py-1 px-2.5 text-xs font-semibold text-white">
+                {mail}
+                <button slot="close-button" on:click={(e)=>removeMail(e,index)} type="button" class="inline-flex ml-2 items-center text-white" aria-label="Remove">
+                  <CloseOutline color="white" class="font-semibold h-4 w-4" />
+                </button>
+              </Badge>
+            {/each}
+            </div>
         </div>
     </form>
         
