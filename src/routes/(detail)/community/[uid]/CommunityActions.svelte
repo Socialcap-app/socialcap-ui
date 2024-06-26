307676a2c0543d7d1e9ca94ef8c21de9c8053b8a
Eric <inux2012@gmail.com>
2024-06-26 11:01:15 -0300

    invite and share

--- src/routes/(detail)/community/[uid]/CommunityActions.svelte
+++ src/routes/(detail)/community/[uid]/CommunityActions.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
   import type { Community } from "$lib/types";
   // 
-  import Time from 'svelte-time';
+  
   import { Button, Badge } from 'flowbite-svelte';
   import { ForwardOutline, CheckOutline } from "flowbite-svelte-icons";
   import CommunityMenu from "$lib/components/communities/CommunityMenu.svelte";
 	import JoinCommunityModal from "$lib/components/communities/JoinCommunityModal.svelte";
+	import CommunityShare from "$lib/components/communities/CommunityShare.svelte";
+	import { goto } from "$app/navigation";
 
   export let community: Community | undefined | null;
 
   let joinModalOpened = false;
+  let openShareModal = false;
 
   function openJoinModal(ev: any) {
     ev.preventDefault();//.stopPropagation();
     joinModalOpened = true;
   }
+
+  console.log("CUID", community)
 </script>
 
 <JoinCommunityModal 
@@ -24,12 +29,19 @@
   bind:open={joinModalOpened} 
 />
 
-<div class="flex items-center space-x-1">
+<div class="flex items-center">
   <!-- 
     <Button size="md" color="light" class="!p-2 --bg-orange-500 --text-white"><EditSolid  class="w-6 h-6" /></Button> 
   -->
-  <Button size="md" color="light" class="p-2 px-4">
-    Share <ForwardOutline class="ms-2 w-5 h-5" />
+  <Button size="md" color="light" class="p-2 px-4 mr-4" 
+    on:click={(e)=>{
+      e.preventDefault()
+      e.stopPropagation()
+      openShareModal = false;
+      openShareModal = true;
+    }
+  }>
+    Share<ForwardOutline class="ms-2 w-5 h-5" />
   </Button> 
 
   {#if !(community?.isMember === '1')}
@@ -43,7 +55,8 @@
   {#if (community?.isMember === '1') && !community?.isAdmin}
     <CommunityMenu 
       isAdmin={community?.isAdmin}
-      joined={community?.isMember === '1'}>
+      joined={community?.isMember === '1'}
+      on:admin={() => goto('/community/admin/' + community?.uid)}>
         <Badge color="light" large class="text-primary-500 border m-0 !py-2 rounded-lg">
           <CheckOutline class="ms-0 w-5 h-5"/>
           Joined
@@ -54,11 +67,14 @@
   {#if community?.isAdmin}
     <CommunityMenu 
       isAdmin={community?.isAdmin}
-      joined={community?.isMember === '1'}>
+      joined={community?.isMember === '1'}
+      on:admin={() => goto('/community/admin/' + community?.uid)}
+      >
         <Button size="md" class="bg-orange-500 p-2 px-4">
-           Admin this
+           Admin
         </Button> 
     </CommunityMenu>  
   {/if}
+  <CommunityShare openModal={openShareModal}/>
 
 </div>
