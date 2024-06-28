3d2205f82d9acc33d191c17842ca545fe2c5ee3b
leomanza <leomanzanal@gmail.com>
2024-06-28 18:42:45 -0300

    fix create plan page and button

--- src/routes/(detail)/plan/admin/[uid]/+page.js
+++ src/routes/(detail)/plan/admin/[uid]/+page.js
@@ -1,6 +1,8 @@
 /** @type {import('./$types').PageLoad} */
-export async function load({ params }) {
+export async function load( { params, route, url })  {
+  const communityUid =  url.searchParams.get('communityUid')
     return {
-      uid: params.uid
+      uid: params.uid,
+      communityUid: communityUid
     };
   }
