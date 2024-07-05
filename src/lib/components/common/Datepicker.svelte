fb282599a0fef15db2ca8ade4004103b00e5ec39
leomanza <leomanzanal@gmail.com>
2024-07-05 20:03:29 -0300

    fix plan creation and update issues

--- src/lib/components/common/Datepicker.svelte
+++ src/lib/components/common/Datepicker.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { Datepicker } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
 	export let name: string,
 		value: Date | undefined,
@@ -7,7 +6,7 @@
 		disabled: boolean = false;
 	placeholder = placeholder ?? 'Pick a date';
 	const datepickerFormat = 'YYYY-MM-DD';
-    let valueString;
+	let valueString;
 	function convertValueForBinding() {
 		const now = new Date();
 		let month = '' + (now.getMonth() + 1),
@@ -18,10 +17,10 @@
 		if (day.length < 2) day = '0' + day;
 
 		valueString = [year, month, day].join('-');
-        console.log("value", valueString)
+		console.log('value', valueString);
 	}
 	onMount(() => {
-		convertValueForBinding();
+		// convertValueForBinding();
 	});
 </script>
 
@@ -40,13 +39,14 @@
 		</svg>
 	</div>
 	<input
+		id={name}
 		datepicker
 		type="date"
 		{name}
 		class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 ps-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
 		{placeholder}
 		datepicker-format={datepickerFormat}
-		bind:value={value}
+		bind:value
 		{disabled}
 		datepicker-buttons
 	/>
