58c3b714038771ea2c72c0b8103542c86b275f5a
leomanza <leomanzanal@gmail.com>
2024-07-07 18:37:31 -0300

    hotfix: fix update plan evidence data

--- src/lib/api/plans-api.ts
+++ src/lib/api/plans-api.ts
@@ -55,7 +55,7 @@ async function getAllClaimables(params: {
  * @returns Updated Plan
  */
 async function updatePlan(data: Plan): Promise<Plan> {
-  const rs = await API.mutate("update_plan", { ...data, state: Number(data.state), evidence: JSON.stringify(data.evidence), startsUTC: convertDateToISO(data.startsUTC), endsUTC: convertDateToISO(data.endsUTC), votingStartsUTC: convertDateToISO(data.votingStartsUTC), votingEndsUTC: convertDateToISO(data.votingEndsUTC) })
+  const rs = await API.mutate("update_plan", { ...data, state: Number(data.state), startsUTC: convertDateToISO(data.startsUTC), endsUTC: convertDateToISO(data.endsUTC), votingStartsUTC: convertDateToISO(data.votingStartsUTC), votingEndsUTC: convertDateToISO(data.votingEndsUTC) })
   
   if (rs.error) throw Error(rs.error.message, rs.error.cause);
   return rs.data;
