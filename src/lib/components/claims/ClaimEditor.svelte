f6a18c1d2e9121331637825082f34661cbd6f97a
Fabricio <fabridp77@gmail.com>
2024-08-13 15:23:12 -0300

    claim-form-err-msg-format

--- src/lib/components/claims/ClaimEditor.svelte
+++ src/lib/components/claims/ClaimEditor.svelte
@@ -14,29 +14,30 @@
 	import { useGetMyCredentials } from '$lib/hooks/credentials';
 	import LoadingSpinner from '../common/LoadingSpinner.svelte';
 	import { InfoCircleSolid } from 'flowbite-svelte-icons';
+	import { fade } from 'svelte/transition';
 
 	export let plan: any, claim: any, isNew: boolean, mode: 'view' | 'edit';
-	const communityQuery = useGetCommunity(plan.communityUid, true)
-	const myCredentialsQuery = useGetMyCredentials()
+	const communityQuery = useGetCommunity(plan.communityUid, true);
+	const myCredentialsQuery = useGetMyCredentials();
 	const saveClaim = useSaveDratClaim();
 	const updateClaim = useUpdateClaim();
-	
+
 	$: communityPlans = $communityQuery.data?.plans;
 	$: myCredentials = $myCredentialsQuery.data;
 
-	let toggleDialog = false,
+	let toggleDialog: boolean = false,
 		step = 0;
 	let previewOn = mode === 'view';
-	let formComponent : any;
-	let formHasErrors : boolean = true;
-	let showFormHasErrorDialog : boolean = false;
+	let formComponent: any;
+	let formHasErrors: boolean = true;
+	let showFormHasErrorDialog: boolean = false;
 
 	async function confirmSubmission() {
 		toggleDialog = !formHasErrors;
 	}
-	
+
 	async function checkFormErrors() {
-		formHasErrors =	formComponent.hasErrors();
+		formHasErrors = formComponent.hasErrors();
 		showFormHasErrorDialog = formHasErrors;
 	}
 
@@ -53,75 +54,105 @@
 		return claim?.state === DRAFT || claim?.state === UNPAID;
 	}
 
-	$:{
-		console.log(claim)
+	$: {
+		console.log(claim);
 	}
 </script>
 
 {#if toggleDialog}
-	<PaymentDialog 
-    bind:open={toggleDialog} 
-    step={PaymentStep.CONFIRM_SUBMIT} 
-    {claim} 
-    {plan} 
-    {isNew} 
-  />
+	<PaymentDialog
+		bind:open={toggleDialog}
+		step={PaymentStep.CONFIRM_SUBMIT}
+		{claim}
+		{plan}
+		{isNew}
+	/>
 {/if}
-{#if $communityQuery.isLoading }
+{#if $communityQuery.isLoading}
 	<LoadingSpinner />
 {:else}
-<div class="relative">
-	{#if showFormHasErrorDialog}
-		<Alert border color="yellow" class="animate-fadeIn fixed top-24 left-1/2 transform -translate-x-1/2 w-4/5 sm:w-3/5">
-			<InfoCircleSolid slot="icon" class="w-5 h-5" />
-			<span class="font-medium">Form Submission Error!</span>
-			Please review the required fields and correct any errors before submitting again.
-		</Alert>
-	{/if}
-	{#if !showFormHasErrorDialog}
-		<Alert border color="yellow" class="animate-fadeOut fixed top-24 left-1/2 transform -translate-x-1/2 w-4/5 sm:w-3/5">
-			<InfoCircleSolid slot="icon" class="w-5 h-5" />
-			<span class="font-medium">Form Submission Error!</span>
-			Please review the required fields and correct any errors before submitting again.
-		</Alert>
-	{/if}
-	<div class="mb-24 w-full p-8">
-		<!-- <pre class="text-xs">
+	<div class="relative">
+		{#if showFormHasErrorDialog}
+			<div in:fade={{ duration: 200 }} out:fade={{ duration: 200 }}>
+				<Alert
+					border
+					color="yellow"
+					class="fixed left-1/2 top-24 w-4/5 -translate-x-1/2 transform sm:w-3/5"
+				>
+					<InfoCircleSolid slot="icon" class="h-5 w-5" />
+					<span class="font-medium">Form Submission Error!</span>
+					Please review the required fields and correct any errors before submitting again.
+				</Alert>
+			</div>
+		{/if}
+		<div class="mb-24 w-full p-8">
+			<!-- <pre class="text-xs">
       {JSON.stringify(plan?.evidence, null, 2)}
     </pre> -->
-		{#if !previewOn}
-			<form>
-				<EvidenceForm bind:this={formComponent} on:validationChange={()=>checkFormErrors()} eform={plan?.evidence} data={claim?.evidenceData} communityUid={plan?.communityUid} {communityPlans} {myCredentials} />
-			</form>
-		{:else}
-			<EvidenceFormPreview evidenceData={claim?.evidenceData} />
-		{/if}
-	</div>
+			{#if !previewOn}
+				<form>
+					<EvidenceForm
+						bind:this={formComponent}
+						on:validationChange={() => checkFormErrors()}
+						eform={plan?.evidence}
+						data={claim?.evidenceData}
+						communityUid={plan?.communityUid}
+						{communityPlans}
+						{myCredentials}
+					/>
+				</form>
+			{:else}
+				<EvidenceFormPreview evidenceData={claim?.evidenceData} />
+			{/if}
+		</div>
 
-	<div class="fixed bottom-0 left-0 right-0 border-t-2 bg-white py-4 lg:left-64">
-		<div class="border-3 --bg-red-300 flex items-center px-8 sm:justify-end justify-center max-[465px]:justify-between flex-wrap gap-4">
-			<div class="sm:absolute sm:left-20">
-				{#if !previewOn}
-					<Button disabled={!isEditable()} alternate color="light" on:click={() => (previewOn = true)}>Preview</Button>
-				{:else}
-					<Button disabled={!isEditable()} alternate color="light" on:click={() => (previewOn = false)}>Edit</Button>
-				{/if}
-			</div>
-			
+		<div class="fixed bottom-0 left-0 right-0 border-t-2 bg-white py-4 lg:left-64">
+			<div
+				class="border-3 --bg-red-300 flex flex-wrap items-center justify-center gap-4 px-8 max-[465px]:justify-between sm:justify-end"
+			>
+				<div class="sm:absolute sm:left-20">
+					{#if !previewOn}
+						<Button
+							disabled={!isEditable()}
+							alternate
+							color="light"
+							on:click={() => (previewOn = true)}>Preview</Button
+						>
+					{:else}
+						<Button
+							disabled={!isEditable()}
+							alternate
+							color="light"
+							on:click={() => (previewOn = false)}>Edit</Button
+						>
+					{/if}
+				</div>
+
 				<span class="me-8 justify-end text-sm sm:mx-3">
 					Status <StateBadge data={claim?.state} />
 				</span>
 
-				<Button disabled={!isEditable()}  size="lg" color="light" class="me-2" on:click={() => saveDraft()}>
+				<Button
+					disabled={!isEditable()}
+					size="lg"
+					color="light"
+					class="me-2"
+					on:click={() => saveDraft()}
+				>
 					{#if $saveClaim.isPending || $updateClaim.isPending}
 						Saving...
 					{:else}
 						Save Draft
 					{/if}</Button
 				>
-				<Button disabled={!isEditable() || formHasErrors} size="lg" primay class="me-8" on:click={() => confirmSubmission()}>Submit</Button>
-			
+				<Button
+					disabled={!isEditable() || formHasErrors}
+					size="lg"
+					primay
+					class="me-8"
+					on:click={() => confirmSubmission()}>Submit</Button
+				>
+			</div>
 		</div>
 	</div>
-</div>
 {/if}
