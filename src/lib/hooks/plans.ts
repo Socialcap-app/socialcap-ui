45d20b19dfb39ada04c0d7477c35f0862a13063a
leomanza <leomanzanal@gmail.com>
2024-10-02 11:12:44 -0300

    fix update active plan on navbar for new plans

--- src/lib/hooks/plans.ts
+++ src/lib/hooks/plans.ts
@@ -1,51 +1,49 @@
-
-import { createMutation, createQuery, useQueryClient } from '@tanstack/svelte-query'
+import { createMutation, createQuery, useQueryClient } from '@tanstack/svelte-query';
 import { getPlan, getAdminedPlans, getAllClaimables, updatePlan, addPlan } from '$lib/api/queries';
 import type { Plan } from '$lib/types/plan';
 import { getPublicClaims } from '$lib/api/plans-api';
 
 export function useGetPlan(uid: string) {
-    return createQuery<Plan, Error>({
-        queryKey: ['get_plan', uid],
-        queryFn: () => getPlan({uid}),
-        enabled: uid !== undefined && uid !== "new"
-      })
+	return createQuery<Plan, Error>({
+		queryKey: ['get_plan', uid],
+		queryFn: () => getPlan({ uid }),
+		enabled: uid !== undefined && uid !== 'new'
+	});
 }
 
 export function useGetAdminedPlans() {
-  return createQuery<Plan[], Error>({
-      queryKey: ['get_admined_plans'],
-      queryFn: () => getAdminedPlans(),
-    })
+	return createQuery<Plan[], Error>({
+		queryKey: ['get_admined_plans'],
+		queryFn: () => getAdminedPlans()
+	});
 }
 
-export function useGetAllClaimables(params: { 
-  joined?: boolean
-}) {
-  return createQuery<Plan[], Error>({
-      queryKey: ['get_my_claimables'],
-      queryFn: () => getAllClaimables(params),
-    })
+export function useGetAllClaimables(params: { joined?: boolean }) {
+	return createQuery<Plan[], Error>({
+		queryKey: ['get_my_claimables'],
+		queryFn: () => getAllClaimables(params)
+	});
 }
 
 export function useUpdatePlan(uid: string | undefined) {
-  const client = useQueryClient();
-  return createMutation({
-    mutationFn: updatePlan,
-    // Always refetch after error or success:
-    onSettled: () => {
-      client.invalidateQueries({ queryKey: ['get_plan', uid] })
-    },
-  })
+	const client = useQueryClient();
+	return createMutation({
+		mutationFn: updatePlan,
+		// Always refetch after error or success:
+		onSettled: () => {
+			client.invalidateQueries({ queryKey: ['get_plan', uid] });
+			client.invalidateQueries({ queryKey: ['get_admined_plans'] });
+		}
+	});
 }
 
 export function useCreatePlan() {
-  const client = useQueryClient();
-  return createMutation({
-    mutationFn: addPlan,
-    // Always refetch after error or success:
-    onSettled: () => {
-  
-    },
-  })
+	const client = useQueryClient();
+	return createMutation({
+		mutationFn: addPlan,
+		// Always refetch after error or success:
+		onSettled: () => {
+			client.invalidateQueries({ queryKey: ['get_admined_plans'] });
+		}
+	});
 }
